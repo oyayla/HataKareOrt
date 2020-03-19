4b96227b7df6011c34116812409d838494b35362
Osman Yayla <osman.yyla@gmail.com>
2020-03-19 15:30:31 +0300

    Update hataKareleri_ort.py

--- hataKareleri_ort.py
+++ hataKareleri_ort.py
@@ -6,6 +6,9 @@ while(i > 0):
 	Gdeger = 0
 	Tdeger = 0
 	topla = 0
+	mutlak = 0
+	mGpT = 0
+	mHKO = 0
 
 	while(a > 0):
 	    a = a - 1
@@ -20,11 +23,19 @@ while(i > 0):
 	    Tdeger = float(input(". Tahmin edilmiş olan değeri gir : "))
 	    GpT = (Gdeger - Tdeger) * (Gdeger - Tdeger)
 	    topla = (topla + GpT)
+	    if((Gdeger - Tdeger) < 0):	
+	    	mGpT = (Gdeger - Tdeger) * -1
+	    else:	
+	    	mGpT = (Gdeger - Tdeger)
 
+	    mutlak = mutlak + mGpT
+
+	mHKO = 1/i * mutlak
 	HKO = 1/i * topla
 
-	print("Hata Karelerinin Ortalaması : ", HKO)
-	print(" Hata karelerinin Ortalamasının Kökü : ", HKO**0.5)
+	print("Hata Karelerinin Ortalaması (MSE) : ", HKO)
+	print("\n Hata karelerinin Ortalamasının Kökü (RMSE) : ", HKO**0.5)
+	print("\n Ortalama mutlak hata (MAE) : ", mHKO)
 
 	i = i - 1
 
